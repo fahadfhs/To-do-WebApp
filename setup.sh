35deb596fc7ce2728d7ef787ba5e09adddf96fb4
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-08-10 13:14:36 -0400

    Add configuration files for deployement on Heroku

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# all these will be updated to heroku
+
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"email@domain\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
